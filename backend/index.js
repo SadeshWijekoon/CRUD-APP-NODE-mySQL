344a87b5e330a5d404eee4ad309e1380093ca4e4
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2024-09-08 15:36:55 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import mysql from 'mysql'
+
+const app =express()
+app.use(express.json())
+
+const db = mysql.createConnection(
+   {
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"test"
+   } 
+)
+app.get('/books',(req,res)=>{
+    const q = 'SELECT * FROM books'
+    db.query(q,(err,data)=>{
+        if (err){
+            res.json(err)
+        }else{
+            res.json(data)
+        }
+    })
+})
+
+app.post('/books',(req,res)=>{
+    const q = 'INSERT INTO books (`title`,`desc`,`cover_pic`)VALUES(?)'
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover_pic
+      ];
+    db.query(q,[values],(err,data)=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json('book has been created ')
+        }
+    })
+})
+
+app.listen(5000,()=>{
+    console.log("backend is connected");
+})
